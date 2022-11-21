b211ebf651bf3ca4e88da773ee46fa4b69c49a95
Guilherme S <guinho.shuto@gmail.com>
2022-11-20 23:41:46 -0400

    slider

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,68 @@
-// import Head from 'next/head'
-import Stories from 'react-insta-stories'
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 
-export default function Home() {
-  const stories = [
-    {
-      url: 'https://cdn.dribbble.com/userupload/3810776/file/original-2a8742b53c412df673c49a7d6c6332e3.png?compress=1&resize=2048x1536',
-    },
-    {
-      url: 'https://cdn.dribbble.com/userupload/4029329/file/original-0e7f081fa340f5544370583687371c62.png?compress=1&resize=1504x1128' ,
-    },
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
-  ]
+import Slide1 from './slides/slide1'
 
+export default function Home(){
   return (
-    <div className=''>
-      <Stories 
-        stories={stories}
-        defaultInterval={1500}
-      />
-    </div>
-  )
-}
+    <Swiper
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+    >
+      <SwiperSlide className="slider"><Slide1 /></SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      ...
+    </Swiper>
+  );
+};
+
+
+// import Head from 'next/head'
+// import Stories from 'react-insta-stories'
+
+// export default function Home() {
+//   const storyStyle = {
+//     width: 'auto',
+//     maxWidth: '100%',
+//     maxHeight: '100vw',
+//     margin: 'auto'
+//   }
+
+  // const stories = [
+  //   {
+  //     url: 'https://cdn.dribbble.com/userupload/3810776/file/original-2a8742b53c412df673c49a7d6c6332e3.png?compress=1&resize=2048x1536',
+  //     styles: storyStyle
+  //   },
+  //   {
+  //     url: 'https://cdn.dribbble.com/userupload/4029329/file/original-0e7f081fa340f5544370583687371c62.png?compress=1&resize=1504x1128' ,
+  //     styles: storyStyle
+  //   },
+
+  // ]
+
+//   return (
+//     <div className=''>
+//       <Stories 
+//         stories={stories}
+//         defaultInterval={1500}
+//         width={'auto'}
+//         height={'100%'}
+//       />
+//     </div>
+//   )
+// }
---
+++ pages/slides/slide1.tsx
@@ -0,0 +1,11 @@
+import styles from '../../styles/slides.module.css'
+
+const Slide1 = () => {
+    return(
+        <div className={styles.slides}>
+            <h1>oi</h1>
+        </div>
+    )
+}
+
+export default Slide1;
