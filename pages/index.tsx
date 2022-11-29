fb3ba0f0bcf17fccd172807ee314f760cf313d58
Guilherme Shuto <guinho.shuto@gmail.com>
2022-11-29 11:57:00 -0400

    preenchimento de variaveis

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useEffect, useState } from 'react';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -10,33 +11,63 @@ import 'swiper/css/scrollbar';
 import Slide1 from './slides/slide1'
 import Slide2 from './slides/slide2'
 
-type props = {
-    username: string;
-    mes: string;
+interface popularSentence{
+  sentence: string;
+  qtd: number;
 }
 
+type dataProps = {
+    username: string;
+    periodo: string;
+    allMessages: number;
+    emojis: Array<popularSentence>;
+    serverEmoji: popularSentence;
+    userPopularWord: popularSentence;
+    serverPopularWord: popularSentence;
+}
+
+// x capa
+// x mandou x mensagens
+// - emoji mais usado
+// - palavra mais falada
+// - por dia
+// - quantos dias vc já está no server
+// - cassino?
+// - mencionado?
+// - resumo/obg
+
 export default function Home(){
-  // const [capaData, setCapaData] = useState<props>({username: '', mes: ''})
+  const [data, setData] = useState<dataProps>({
+    'username': 'sainas',
+    'periodo': 'novembro',
+    'allMessages': 1221,
+    'emojis': [{sentence: 'a', qtd: 1},{sentence: 'b', qtd: 2}, {sentence:'c', qtd:3}],
+    'serverEmoji': {sentence: 'a', qtd: 1},
+    'userPopularWord': {sentence: 'oi', qtd: 1},
+    'serverPopularWord': {sentence: 'ciao', qtd: 1},
+  })
 
   // useEffect(() => {
-  //   setCapaData({
-  //     username: 'sainas', 
-  //     mes: 'novembro',
-  //   }, [])
-  // })
+
+  // }, )
+
   const capaData = {
-    username: 'sainas',
-    mes: 'novembro'
+    username: data.username,
+    mes: data.periodo
   }
 
   const mensagens = {
-    periodo: 'novembro',
-    qtd: 11214
+    periodo: data.periodo,
+    qtd: data.allMessages
+  }
+
+  const emojis = {
+    emojis: data.emojis,
+    serverEmoji: data.serverEmoji
   }
 
   return (
     <Swiper
-      // install Swiper modules
       modules={[Navigation, Pagination, Scrollbar, A11y]}
       spaceBetween={50}
       slidesPerView={1}
@@ -53,39 +84,3 @@ export default function Home(){
     </Swiper>
   );
 };
-
-
-// import Head from 'next/head'
-// import Stories from 'react-insta-stories'
-
-// export default function Home() {
-//   const storyStyle = {
-//     width: 'auto',
-//     maxWidth: '100%',
-//     maxHeight: '100vw',
-//     margin: 'auto'
-//   }
-
-  // const stories = [
-  //   {
-  //     url: 'https://cdn.dribbble.com/userupload/3810776/file/original-2a8742b53c412df673c49a7d6c6332e3.png?compress=1&resize=2048x1536',
-  //     styles: storyStyle
-  //   },
-  //   {
-  //     url: 'https://cdn.dribbble.com/userupload/4029329/file/original-0e7f081fa340f5544370583687371c62.png?compress=1&resize=1504x1128' ,
-  //     styles: storyStyle
-  //   },
-
-  // ]
-
-//   return (
-//     <div className=''>
-//       <Stories 
-//         stories={stories}
-//         defaultInterval={1500}
-//         width={'auto'}
-//         height={'100%'}
-//       />
-//     </div>
-//   )
-// }
