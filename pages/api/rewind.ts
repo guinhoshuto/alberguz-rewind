f62f99a536f35f90074cb568268658cbd2969f41
Guilherme Shuto <guinho.shuto@gmail.com>
2022-12-09 10:12:19 -0400

    month and channel

--- pages/api/rewind.ts
+++ pages/api/rewind.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+// const buffer = new Buffer(blob)
 
 require('dotenv').config()
 const mysql = require('mysql2/promise')
@@ -24,15 +25,19 @@ export default async function handler(
     //allmessages
     const userData = user.find((u:any) => u.user_id === id)
     const serverData = user.find((u:any) => u.user_id === '1')
-    console.log(userData, serverData)
-    
+    // console.log(userData, serverData)
+    // userData.messages_by_period.text().then((a: any) => console.log(a))
+    // console.log(userData.messages_by_period.text())
+
+
     // res.json(user)
     res.status(200).json({
       username: userData.username,
       periodo: periodo,
       allMessages: parseInt(userData.all_messages),
       serverAllMessages: parseInt(serverData.all_messages),
-
+      messagesByMonth: Buffer.from(userData.messages_by_period).toString(),
+      messagesByChannel: Buffer.from(userData.messages_by_channel).toString()
     })
   })
   .catch((e: any) => res.status(500).json(e))
