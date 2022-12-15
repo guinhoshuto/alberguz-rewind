15129d05eadedbc9541bb4d4937560b1acc63176
Guilherme S <guinho.shuto@gmail.com>
2022-12-14 23:12:12 -0400

    sei lá

--- pages/slides/messagesByChannel.tsx
+++ pages/slides/messagesByChannel.tsx
@@ -2,24 +2,31 @@ import styles from '../../styles/Slides.module.css'
 import { useEffect, useState } from 'react';
 import BarChart from '../components/BarChart';
 
-// type props = {
-//     qtd: number
-//     qtdAll: number
-//     periodo: string
-// }
+type props = {
+    key: string;
+    value: number
+}
 
-const MessagesByChannel = (messagesByChannel: any) => {
+const MessagesByChannel = (messagesByChannel: props[]) => {
     // const [data, setData] = useState<props>({qtd: 0, qtdAll: 0, periodo: ''})
 
     // useEffect(() => {
     //     setData(messagesByChannel);
     // } )
     // console.log(data)
+    const channelMessages = Object.entries(messagesByChannel).map((m: any) => m[1].value)
+    channelMessages.pop()
+
+    const mostMessagedChannel: any = Object.entries(messagesByChannel).find((m: any) => m[1].value === Math.max(...channelMessages))
 
     return(
         <div className={styles.slides}>
             <div className='flex items-center justify-items-center h-full w-full'>
                 <div className='px-8 text-center w-full'>
+                    <h1>
+                        {/* Em <span className='text-azoxo'>2022</span> você enviou <span className='text-azoxo'>{mostMessagedChannel[1].value}</span><br/>
+                        no canal <span className='text-azoxo'>{mostMessagedChannel[1].key}</span> */}
+                    </h1>
                     {/* <p className='md:text-2xl sm:text-xl font-black'>Em <span className="text-azoxo">{data.periodo}</span>, você enviou</p>
                     <p className='text-azoxo md:text-[220px] sm:text-6xl font-black'>{new Intl.NumberFormat('pt-BR').format(data.qtd)}</p>
                     <p className='md:text-2xl sm:text-xl font-black'>mensagens</p> */}
--- pages/slides/messagesByPeriod.tsx
+++ pages/slides/messagesByPeriod.tsx
@@ -1,20 +1,58 @@
 import styles from '../../styles/Slides.module.css'
 import { useEffect, useState } from 'react';
 import BarChart from '../components/BarChart'
+import { StringLiteral } from 'typescript';
 
-const MessagesByPeriod = (messagesByPeriod: any) => {
+type props = {
+    key: string;
+    value: number
+}
+
+type monthProp = {
+    key: string;
+    value: string
+}
+
+const MessagesByPeriod = (messagesByPeriod: props[]) => {
     // const [data, setData] = useState<any>({jan: 0, fev: 0, mar: 0, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0})
-
-    // useEffect(() => {
-    //     setData(messagesByPeriod)
-    // }, )
-    // console.log(capaData)
+    const months = [
+        {key: 'jan', month: 'Janeiro'},
+        {key: 'fev', month: 'Fevereiro'},
+        {key: 'mar', month: 'Março'},
+        {key: 'abr', month: 'Abril'},
+        {key: 'mai', month: 'Maio'},
+        {key: 'jun', month: 'Junho'},
+        {key: 'jul', month: 'Julho'},
+        {key: 'ago', month: 'Agosto'},
+        {key: 'set', month: 'Setembro'},
+        {key: 'out', month: 'Outubro'},
+        {key: 'nov', month: 'Novembro'},
+    ]
+    // // useEffect(() => {
+    // //     setData(messagesByPeriod)
+    // // }, )
+    // // console.log(capaData)
+    // // console.log(messagesByPeriod)
+    // // const mostMessagedMonth = Math.max(...Object.keys(messagesByPeriod).map((m: any) => m.value))
+    // // console.log(mostMessagedMonth)
     console.log(messagesByPeriod)
+
+    const messagesCounts = Object.values(messagesByPeriod).map((m: props) => m.value)
+    console.log(messagesCounts)
+    messagesCounts.pop()
+    const mostMessagedMonth: props = Object.values(messagesByPeriod).find((m: any) => m.value === Math.max(...messagesCounts))
+    const monthName: monthProp = months.find((m: monthProp) => m.key === mostMessagedMonth?.key)
+    // mostMessagedMonth.monthName = monthName?.month
+    console.log(mostMessagedMonth)
 
     return(
         <div className={styles.slides}>
             <div className='flex items-center justify-items-center h-full w-full'>
                 <div className='px-8 text-center w-full'>
+                    <h1 className="text-4xl font-black mb-16">
+                        <span className='text-azoxo'>{monthName}</span> foi o mês em que você mais mandou mensagens, <br/>
+                         foram <span className="text-azoxo">{new Intl.NumberFormat('pt-BR').format(mostMessagedMonth.value)}</span>!!!!!!!
+                    </h1>
                     {/* <p className='md:text-2xl sm:text-xl font-black'>Em <span className="text-azoxo">{data.periodo}</span>, você enviou</p>
                     <p className='text-azoxo md:text-[220px] sm:text-6xl font-black'>{new Intl.NumberFormat('pt-BR').format(data.qtd)}</p> */}
                     <BarChart {...messagesByPeriod} />
